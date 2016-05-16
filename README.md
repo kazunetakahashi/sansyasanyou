698588846a5f2438261e6ca3194c849e0bf922a3
Kazune Takahashi <kazunetakahashi@gmail.com>
2016-05-16 14:47:54 +0900

    fixed

---
+++ README.md
@@ -0,0 +1,8 @@
+# sansyasanyou
+2016年5月16日から、TVアニメ『三者三葉』公式サイトで配布されている[「Twitterアイコン」](http://sansyasanyou.com/special/twitter.html)333個を全てダウンロードするためのプログラムです。
+
+Ruby 処理系で `twitter_icon_dl.rb` を実行すると、`twitter_icon_dl.rb` からみて `../icons/` 下にダウンロードされます。
+
+`twitter_icon_dl.rb` を実行した際にダウンロードされていない画像のみをダウンロードするようになっています。
+
+配布が開始されてからしばらくは、サーバが落ちていました。申し上げるまでもありませんが、くれぐれも先方への攻撃とならないようにお願いします。
--- twitter_icon_dl.rb
+++ twitter_icon_dl.rb
@@ -4,19 +4,25 @@
 require 'open-uri'
 require 'fileutils'
 
-dir = './icons/'
+dir = File.expand_path('../icons/', __FILE__)
 
 if !FileTest.exist?(dir)
-  FileUtils.mkdir_p(dir)  
+  FileUtils.mkdir_p(dir)
+  puts "ディレクトリ #{dir} を作成しました。"
 end
 
 for i in 1..333
   url = "http://sansyasanyou.com/core_sys/images/main/cont/special/ico/#{i}.png"
   name = File.basename(url)
-  open(dir+name, 'w') {|output|
+  file = dir + '/' + name
+  if FileTest.exist?(file)
+    # puts "#{name} はスキップされました。"
+    next
+  end
+  open(file, 'w') {|output|
     open(url) {|input|
       output.write(input.read)
+      puts "#{name} をダウンロードしました。"
     }
   }
 end
-
